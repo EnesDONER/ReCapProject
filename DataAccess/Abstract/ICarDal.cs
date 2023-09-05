9a24da30e7596762ef8897d726e9e151095f47df
Enes Doner <businessenesdoner@gmail.com>
2023-09-05 18:28:37 +0300

    docker example

--- DataAccess/Abstract/ICarDal.cs
+++ DataAccess/Abstract/ICarDal.cs
@@ -8,7 +8,7 @@ using System.Linq.Expressions;
 using DataAccess.Dtos;
 
 namespace DataAccess.Abstract
-{//CRUD işlemleri IEntityRepository tipinde referansları tutan interfacede tanıtıldı(Core->DataAccess)
+{ 
     public interface ICarDal:IEntityRepository<Car>
     {
         List<CarDetailDto> GetCarDetails(Expression<Func<CarDetailDto, bool>> filter = null);
